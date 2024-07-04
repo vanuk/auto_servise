c193423a18c13d6a0628d775f0a15ad60b02505e
vanuk <93387425+vanuk@users.noreply.github.com>
2024-07-04 18:15:00 +0300

    додав емаіл запити

--- code.js
+++ code.js
@@ -10,3 +10,17 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+function sendEmail()
+{
+    let params={
+        from_name: document.getElementById('fromName').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+    }
+    emailjs.send("service_8t3tgp9","template_vz64q9t", params)
+    .then(function (res) 
+    {
+        alert("Message sent successfully" + res.status);
+    });
+
+}

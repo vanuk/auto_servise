636cced249cd5426e7d77172fb37d2d83013dbfc
liking1 <tlvlad57@gmail.com>
2024-08-05 13:10:20 +0300

    Зробили розділ галереї та добавили майже на всі сторінки хедери. Інші фікси

--- code.js
+++ code.js
@@ -1,15 +1,3 @@
-// Add event listener to navigation menu
-document.addEventListener("DOMContentLoaded", function() {
-    const nav = document.querySelector("nav");
-    nav.addEventListener("click", function(event) {
-        if (event.target.tagName === "A") {
-            event.preventDefault();
-            const targetId = event.target.getAttribute("href").substring(1);
-            const targetSection = document.getElementById(targetId);
-            targetSection.scrollIntoView({ behavior: "smooth" });
-        }
-    });
-});
 function sendEmail()
 {
     let params={
@@ -24,3 +12,28 @@ function sendEmail()
     });
 
 }
+
+var counter = 0;
+    var srcArray = ["/photos/car_mechanic..jpg",
+        "/photos/car_mechanic2..jpg",
+        "/photos/car_mechanic3..jpg"];
+        
+    function updateImages() {
+        var prevIndex = (counter - 1 + srcArray.length) % srcArray.length;
+        var nextIndex = (counter + 1) % srcArray.length;
+        document.getElementById("prevImage").src = srcArray[prevIndex];
+        document.getElementById("currentImage").src = srcArray[counter];
+        document.getElementById("nextImage").src = srcArray[nextIndex];
+    }  
+
+    document.getElementById("prev").onclick = function() {
+        counter = (counter - 1 + srcArray.length) % srcArray.length;
+        updateImages();
+    }
+
+    document.getElementById("next").onclick = function() {
+        counter = (counter + 1) % srcArray.length;
+        updateImages();
+    }
+
+    updateImages();
